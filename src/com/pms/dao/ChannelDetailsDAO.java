03f1f5106dda3ef081a3666cbdc9a524a91fa648
ashutosh Singh <ashutosh.tct@gmail.com>
2019-01-01 10:38:29 +0530

    channel details added

---
+++ src/com/pms/dao/ChannelDetailsDAO.java
@@ -0,0 +1,11 @@
+package com.pms.dao;
+
+import com.pms.util.DBConstants;
+/**
+ * 
+ * @author DELL
+ *
+ */
+public class ChannelDetailsDAO implements DBConstants{
+
+}
---
+++ src/com/pms/entity/ChannelDetails.java
@@ -0,0 +1,37 @@
+package com.pms.entity;
+/**
+ * 
+ * @author DELL
+ *
+ */
+public class ChannelDetails {
+
+	private Integer channelId;
+	private String channelName;
+	private Integer channelFees;
+
+	public Integer getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(Integer channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public Integer getChannelFees() {
+		return channelFees;
+	}
+
+	public void setChannelFees(Integer channelFees) {
+		this.channelFees = channelFees;
+	}
+
+}
